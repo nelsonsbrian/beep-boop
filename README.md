a00e281dee52786b9deea9729053b55e68e4a21e
Brian Nelson <nelsonsbrian@gmail.com>
2018-08-17 08:37:55 -0700

    add README and update with specs

---
+++ README.md
@@ -0,0 +1,49 @@
+# _Beep Boop_
+
+#### _Beep Boop_, August 17, 2018_
+
+#### By _**Brian Nelson**_
+
+## Description
+
+_Simple webage that takes a user number input and has a robot respond to it._
+
+## Spec
+
+| Spec | Input | Output |
+| :-------------     | :------------- | :------------- |
+| **The program returns the same user input back.** | Input: "3" | Output: "3" |
+| **The program only allows numeric inputs.** | Input: "hello" | Output: "beepboop???" |
+| **The program replaces 0 with "Beep!".** | Input: "0" | Output: "Beep!" |
+| **The program replaces 1 with "Boop!".** | Input: "1" | Output: "Boop" |
+| **The program returns a response of "I'm sorry, Dave. I'm afraid I can't do that." if the user input is divisible by 3.** | Input: "9" | Output: "I'm sorry, Dave. I'm afraid I can't do that." |
+
+
+
+## Setup/Installation Requirements
+
+* Open file in a web browser.
+* Clone the repository.
+## Known Bugs
+
+_None._
+
+## Support and contact details
+
+_nelsonbrian@gmail.com_
+
+## Technologies Used
+
+_HTML
+
+Atom.
+
+JavaScript
+
+GitHub_
+
+### License
+
+*This is licensed under MIT.*a
+
+Copyright (c) 2018 **_Brian Nelson_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,6 @@
 // business logic
 var encrypt = function (inputString){
 
-  //variable setup
-  inputString = inputString.replace(/\W/g, '');
-  stringLength = inputString.length;
-  var squareString = Math.sqrt(stringLength);
-  var encryptArr = [];
-  var row = Math.ceil(squareString);
-  var column = Math.ceil(stringLength/row);
-
-  //Encryption
-  for (i=0, j=0, k=0; i<stringLength*column; i+=column, k+=column) {
-    if (k >= stringLength) {
-      j++;
-      k = j;
-    }
-    encryptArr.push(inputString[k % stringLength]);
-  }
-
-  //formatting into 5 letters per word from 1 string
-  var output = encryptArr.join('');
-  alert(output);
-  output = output.match(/.{1,5}/g,).toString();
-  output = output.replace(/,/g," ");
-  return(output);
 
 }
 // user logic
@@ -35,25 +12,3 @@ $(document).ready(function() {
     $("#result").text(output);
   });
 });
-
-
-
-
-
-
-
-
-
-
-// alert(stringLength + " " + row + " " + column);
-// for (index = 0, k=0, j=0; index < Math.ceil(stringLength / 5); index++){
-//   for (i = 0; i < column * 5; i+= column, k++) {
-//     if (stringLength < (k * column)) {
-//       j++;
-//       k = j;
-//       alert(i + " " + k + " " + inputString[(k * column)]);
-//     }
-//     encryptArr.push(inputString[(k * column)]);
-//   }
-//   alert(encryptArr);
-// }
